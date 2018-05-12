6353aa70d8b336003afed8803dfbd915aa1e7a6c
Loïc Fonkam <fonkamloic@gmail.com>
2018-05-12 22:36:55 +0100

    Adding some new dorectories

---
+++ scr/double_linked_list.c
@@ -0,0 +1,12 @@
+/* Title 	: double_linked_list.c
+ * Date 	: 2018-05-10
+ * Author	: "Fonkam loic" <fonkamloic@gmail.com>
+ * Description	: implementation of a generic double_linked_list for code reused
+ * version	: C17
+ * option	: none
+ */
+
+#include <stdio.h>
+
+
+

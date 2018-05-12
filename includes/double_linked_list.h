c80a4437e35b03a35242c3aede1a1fe66dedde0e
Loïc Fonkam <fonkamloic@gmail.com>
2018-05-12 22:56:13 +0100

    Some more documentation

---
+++ includes/double_linked_list.h
@@ -0,0 +1,10 @@
+/* Title 	: double_linked_list.h
+ * Date 	: 2018-05-10
+ * Author	: "Fonkam loic" <fonkamloic@gmail.com>
+ * Description	: double_linked_list.c interface
+ * version	: C17
+ * option	: none
+ */
+
+
+
